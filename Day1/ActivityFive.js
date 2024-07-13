2ad073412f3768a396db77eed40540496ccab420
SojibHasan <sojibsrd85@gmail.com>
2024-07-13 23:54:43 +0600

    activity five completed

---
+++ Day1/ActivityFive.js
@@ -0,0 +1,9 @@
+// Understanding Const 
+// Task 1: Try Reassigning a variable declared with const and observe the error
+
+
+// Solution
+const country = "Bangladesh"
+console.log(country)
+
+country = "India"
