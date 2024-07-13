6cc71472575ca869712ca63a8da8436aed4f7d36
SojibHasan <sojibsrd85@gmail.com>
2024-07-13 23:48:48 +0600

    activity three completed

---
+++ Day1/ActivityThree.js
@@ -0,0 +1,16 @@
+// Data Types
+//Task 1: Create a variable of different data types(number, string, boolean, object, array) and log each variable's type using the typeof operator.
+
+
+//Solution:
+const num = 10
+const str = "Hello World"
+const bol = true
+const obj = {country: "Bangladesh"}
+const arr = [1,2,3,4]
+
+console.log(typeof num)
+console.log(typeof str)
+console.log(typeof bol)
+console.log(typeof obj)
+console.log(typeof arr)
