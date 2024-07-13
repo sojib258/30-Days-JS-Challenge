5564066aca01b107a4742dbb3c27ba62f3fb1f15
SojibHasan <sojibsrd85@gmail.com>
2024-07-13 23:45:27 +0600

    activity one completed

---
+++ Day1/ActivityOne.js
@@ -0,0 +1,15 @@
+// Variable Declaration
+//Task 1: Declare a variable using var, assign it a number, and log the value to the console
+
+// Task 2: Declare a variable using let, assign it a string, and log the valut to the console
+
+
+// Solution
+// Task 1:
+
+var num = 20
+console.log(num)
+
+// Task 2:
+let str = "Hello World"
+console.log(str)
