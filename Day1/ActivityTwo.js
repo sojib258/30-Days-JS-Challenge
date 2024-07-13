c3472079eeb9e30672f74963461bd78a5fbd5223
SojibHasan <sojibsrd85@gmail.com>
2024-07-13 23:46:56 +0600

    activity two completed

---
+++ Day1/ActivityTwo.js
@@ -0,0 +1,6 @@
+// Constant Declaration
+//Task 1: Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+//Solution:
+const boolean = true
+console.log(boolean)
