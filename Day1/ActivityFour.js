d2ed3477a2bd138274dcf3e00e4dd5e8651ece64
SojibHasan <sojibsrd85@gmail.com>
2024-07-13 23:52:01 +0600

    activity four completed

---
+++ Day1/ActivityFour.js
@@ -0,0 +1,10 @@
+// Reassigning Variables
+// Task 1: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+
+
+// Solution:
+let country = "Bangladesh"
+console.log(country)
+
+country = "India"
+console.log(country)
