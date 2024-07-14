cd5293fd823e82c93fae7e252d3e42db1da04fdf
SojibHasan <sojibsrd85@gmail.com>
2024-07-14 20:30:19 +0600

    day2 activity five completed

---
+++ Day2/ActivityFive.js
@@ -0,0 +1,11 @@
+// Ternaray Operator
+// Task 1: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+
+
+// Solution
+const number = 8
+
+const result = number >= 0 ? "Positive" : "Negative"
+
+console.log(result)
