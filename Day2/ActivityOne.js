a403db46264c0044a7add29cef0c9c42242b71ad
SojibHasan <sojibsrd85@gmail.com>
2024-07-14 19:33:04 +0600

    day2 activity one completed

---
+++ Day2/ActivityOne.js
@@ -0,0 +1,45 @@
+// Arithmetic Operator
+// Task 1: Write a program to add two numbers and log the result to the console.
+
+// Task 2: Write a program to subtract two numbers and log the result to the console.
+
+// Task 3: Write a program to multiply two numbers and log the result to the console.
+
+// Task 4: Write a program to divide two numbers and log the result to the console.
+
+// Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console
+
+
+
+// Solution
+// Task 1: 
+const numberOne = 15
+const numberTwo = 10
+
+const result = numberOne + numberTwo
+console.log(result)
+
+
+// Task 2:
+const result2 = numberOne - numberTwo
+console.log(result2)
+
+// Task 3:
+const result3 = numberOne * numberTwo
+console.log(result3)
+
+// Task 4:
+const result4 = numberOne / numberTwo
+console.log(result4)
+
+// Task 5:
+const result5 = numberOne % numberTwo
+console.log(result5)
+
+
+
+
+
+
+
+
