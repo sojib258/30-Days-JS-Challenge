6710178454fc1fd617c8aa19f84eefea5f6d4d5a
SojibHasan <sojibsrd85@gmail.com>
2024-07-14 19:52:45 +0600

    day2 activityThree completed

---
+++ Day2/ActivityThree.js
@@ -0,0 +1,33 @@
+// Comparison Operator
+// Task 1: Write a program to compare two numbers using > and < and log the result to the console.
+
+// Task 2: Write a program to compare two numbers using >= and <= and log the result to the console.
+
+// Task 3: Write a program to compare two numbers using == and === and log the result to the console.
+
+
+
+
+// Solution
+// Task 1:
+const numOne = 20
+const numTwo = 30
+const result = numOne > numTwo
+const resultTwo = numOne < numTwo
+console.log(result)
+console.log(resultTwo)
+
+
+// Task 2: 
+const resultThree = numOne >= numTwo
+const resultFour = numOne <= numTwo
+console.log(resultThree)
+console.log(resultFour)
+
+
+
+// Task 3: 
+const resultFive = numOne == numTwo
+const resultSix = numOne === numTwo
+console.log(resultFive)
+console.log(resultSix)
---
+++ Day2/ActivityTwo.js
@@ -0,0 +1,20 @@
+// Assignment Operator
+// Task 1: Use the += operator to add a number to a variable and log the result to the console.
+
+// Task 2: Use the -= operator to subtract a number from a variable and log the result to the console.
+
+
+
+
+
+// Solution
+// Task 1:
+let numOne = 30
+numOne += 25
+console.log(numOne)
+
+
+// Task 2
+let numThree = 60
+numThree -= 30
+console.log(numThree)
