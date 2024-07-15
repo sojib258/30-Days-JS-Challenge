c902fe2c1ac3d29d9ae5f7c475595b7ef08b00a7
SojibHasan <sojibsrd85@gmail.com>
2024-07-15 18:05:01 +0600

    Voting Eligibility Script Completed

---
+++ Day3/votingScript.js
@@ -0,0 +1,9 @@
+// Task 2: Write a program to check if a person is eligible to vote (age >= 18)  and log the result to the console.
+
+
+const age = 16
+if(age >= 18){
+  console.log("Person Eligible to Vote")
+}else{
+  console.log("Person Are Not Eligible to Vote")
+}
