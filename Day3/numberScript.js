1dd238eb713f353c0502934335f47c8741de07eb
SojibHasan <sojibsrd85@gmail.com>
2024-07-15 18:00:29 +0600

    number check script completed

---
+++ Day3/numberScript.js
@@ -0,0 +1,29 @@
+// If-Else Statements
+// Task 1: Write a program to check if a number is positive, negative, or zero and log the result to the console.
+
+
+// Solution: 
+// Task 1: 
+
+const number = 10
+if(number > 0){
+  console.log("Positive")
+}else if(number < 0){
+  console.log("Negative")
+}else{
+  console.log("Zero")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
